8dd033839ec91252b96a3eb7926562ee6e0026ec
vlad <vladlisin2@gmail.com>
2021-11-23 18:45:53 +0700

    keys

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -6,7 +6,9 @@ interface IKeyboardProps {
 }
 
 const Keyboard: React.FunctionComponent<IKeyboardProps> = (props) => {
-  const letters = 'А.Б.В.Ы.А.Б.В.Ы.А.Б.В.Ы.А.Б.В.Ы.А.Б.В.Ы.А.Б.В.Ы.А.Б.В.Ы.А.Б.В.Ы.А.Б.В.Ы'
+  const letters = 'ӑ.ӱ.ў.ӧ.ө.є.ә.i.ү.ң.ӈ.ҥ.њ.қ.ӄ.ғ.ӌ.һ.č.ӆ.ԓ.љ.ʔ.ʕ.Ӱ.ӱ.Ö.ӧ.Ҥ.ҥ.Ғ.ғ.Ң.ң.I.i.Қ.қ.Һ.H.Ү.ү.Ө.ɵ.Č.č.ʔ.ʕ.Ӄ.ӄ.Ӈ.ӈ.ә'
+
+
   return <>
     <div className='keyboard-container'>
       {letters.split('.').map(l => {
